5d66f9b7c6410b28cfa436607d1c41bcf6b95072
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-02-08 18:12:30 +0300

    33

--- lib/home.dart
+++ lib/home.dart
@@ -5,39 +5,48 @@ import 'package:riverpodexample2/statenotifier.dart';
 
 final myStateNotifierProvider = StateNotifierProvider((ref) => MyStateNotifier(0));
 
-class Home extends ConsumerWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatelessWidget {
+  const Home({super.key});
 
+
+ 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    var counter = ref.watch(myStateNotifierProvider);
+  Widget build(BuildContext context) {
+  
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            counter.toString(),
-            style: const TextStyle(fontSize: 40),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                
-                ref.read(myStateNotifierProvider.notifier).increment();
-              },
-              child: const Text('Increment')),
-          
-           ElevatedButton(
-              onPressed: () {
-                
-                ref.read(myStateNotifierProvider.notifier).decrement();
-              },
-              child: const Text('Decrement')),
-        ],
-      )),
+      body: Consumer(
+        builder: (BuildContext context, WidgetRef ref, Widget? child) { 
+
+            return Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+               ref.watch(myStateNotifierProvider).toString(),
+              style: const TextStyle(fontSize: 40),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  
+                  ref.read(myStateNotifierProvider.notifier).increment();
+                },
+                child: const Text('Increment')),
+            
+             ElevatedButton(
+                onPressed: () {
+                  
+                  ref.read(myStateNotifierProvider.notifier).decrement();
+                },
+                child: const Text('Decrement')),
+          ],
+        ));
+
+         },
+       
+      ),
     );
   }
 }
