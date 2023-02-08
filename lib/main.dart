25e0e05c42fb68f68ffd3964e75feb3a86896e24
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-02-08 18:15:02 +0300

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Home(),
